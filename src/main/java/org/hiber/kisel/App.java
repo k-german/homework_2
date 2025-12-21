3cf761421e46c589f38814d5f439d1a3fe7d9362
german_kiselev <g.a.kiselev@gmail.com>
2025-12-21 03:55:47 +0300

    init

---
+++ src/main/java/org/hiber/kisel/App.java
@@ -0,0 +1,13 @@
+package org.hiber.kisel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
