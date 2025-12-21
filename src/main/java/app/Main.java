467ae687b8b6a0abd4b844a968a87bfbd1eabc87
german_kiselev <g.a.kiselev@gmail.com>
2025-12-21 05:37:21 +0300

    add logging, test logging

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,13 @@
 package app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("START");
+    }
 }
