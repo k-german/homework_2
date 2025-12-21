25fcc46ad188aea1af484087b5cbf9c952284356
german_kiselev <g.a.kiselev@gmail.com>
2025-12-21 06:37:14 +0300

    add connection in Main. correct hibernate.cfg.xml (database name, username)

--- src/main/java/org/hiber/kisel/app/Main.java
+++ src/main/java/org/hiber/kisel/app/Main.java
@@ -1,5 +1,7 @@
 package org.hiber.kisel.app;
 
+import org.hiber.kisel.utils.HibernateUtil;
+import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,5 +11,19 @@ public class Main {
 
     public static void main(String[] args) {
         log.info("START");
+
+        try (Session session = HibernateUtil
+                .getSessionFactory()
+                .openSession()) {
+
+            session.beginTransaction();
+            log.info("Hibernate session opened - success");
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            log.error("Hibernate session NOT opened, exception:", e);
+        } finally {
+            HibernateUtil.shutdown();
+        }
     }
 }
