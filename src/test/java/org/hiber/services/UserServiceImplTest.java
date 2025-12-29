3715674a46d40d49f83e5ec8fc664ed545f29685
german_kiselev <g.a.kiselev@gmail.com>
2025-12-29 13:06:16 +0300

    first unit test done: deleteByID_NullId_throwsBusinessException.

---
+++ src/test/java/org/hiber/services/UserServiceImplTest.java
@@ -0,0 +1,28 @@
+package org.hiber.services;
+
+import org.hiber.dao.UserDao;
+import org.hiber.services.exceptions.BusinessException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserDao userDao;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void deleteById_nullId_throwsBusinessException() {
+        assertThrows(BusinessException.class, () -> userService.deleteById(null));
+        verifyNoInteractions(userDao);
+    }
+}
