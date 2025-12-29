281b23f3cd5981f1948c76f704f688ad84da3234
german_kiselev <g.a.kiselev@gmail.com>
2025-12-29 13:53:50 +0300

    deleteByID valid Id - test done.

--- src/test/java/org/hiber/services/UserServiceImplTest.java
+++ src/test/java/org/hiber/services/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package org.hiber.services;
 
 import org.hiber.dao.UserDao;
 import org.hiber.services.exceptions.BusinessException;
+import org.hiber.services.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,7 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -36,6 +37,14 @@ class UserServiceImplTest {
     void deleteById_negativeId_throwsBusinessException() {
         assertThrows(BusinessException.class, () -> userService.deleteById(-5));
         verifyNoInteractions(userDao);
+    }
+
+    @Test
+    void deleteById_validId_userNotFound_throwsUserNotFoundException() {
+        Integer validId = 50;
+        when(userDao.deleteById(validId)).thenReturn(0);
+        assertThrows(UserNotFoundException.class, () -> userService.deleteById(validId));
+        verify(userDao, times(1)).deleteById(validId);
     }
 
 }
