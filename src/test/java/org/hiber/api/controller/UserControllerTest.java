8b123d391162efae7dbc00792ef9ec9ff5a35715
german_kiselev <g.a.kiselev@gmail.com>
2026-01-14 11:30:43 +0300

    change version of spring-boot-starter-parent to 3.5.9. add UserControllerTest

---
+++ src/test/java/org/hiber/api/controller/UserControllerTest.java
@@ -0,0 +1,28 @@
+package org.hiber.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hiber.api.mapper.UserMapper;
+import org.hiber.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private UserMapper userMapper;
+}
+
