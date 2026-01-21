748f38842bef59b72e74902c60ec02d725b9dc83
german_kiselev <g.a.kiselev@gmail.com>
2026-01-21 11:43:18 +0300

    UserNotificationProducer.java code added and tested

--- src/main/java/org/hiber/kafka/producer/UserNotificationProducer.java
+++ src/main/java/org/hiber/kafka/producer/UserNotificationProducer.java
@@ -1,4 +1,21 @@
 package org.hiber.kafka.producer;
 
+import org.hiber.kafka.dto.UserNotificationEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserNotificationProducer {
+
+    private static final String TOPIC = "user.notifications";
+
+    private final KafkaTemplate<String, UserNotificationEvent> kafkaTemplate;
+
+    public UserNotificationProducer(KafkaTemplate<String, UserNotificationEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(UserNotificationEvent event) {
+        kafkaTemplate.send(TOPIC, event.getEmail(), event);
+    }
 }
