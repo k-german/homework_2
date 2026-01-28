435e76d2bb886f5a2771adc22b44aa9ffa237929
german_kiselev <g.a.kiselev@gmail.com>
2026-01-28 19:09:17 +0300

    add swagger

--- src/main/java/org/hiber/api/dto/UserRequestDto.java
+++ src/main/java/org/hiber/api/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package org.hiber.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -10,13 +11,16 @@ import lombok.Setter;
 @Getter
 public class UserRequestDto {
 
+    @Schema(description = "Имя пользователя")
     @NotBlank(message = "name must not be blank")
     private String name;
 
+    @Schema(description = "Email пользователя")
     @NotBlank(message = "email must not be blank")
     @Email(message = "is not valid")
     private String email;
 
+    @Schema(description = "Возраст пользователя")
     @PositiveOrZero(message = "must be >= 0")
     private Integer age;
 
