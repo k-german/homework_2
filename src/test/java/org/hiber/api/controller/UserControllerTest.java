3457d6d2676aee8c70a6dce85b0abe52af975317
german_kiselev <g.a.kiselev@gmail.com>
2026-02-08 23:56:08 +0300

    config

--- src/test/java/org/hiber/api/controller/UserControllerTest.java
+++ src/test/java/org/hiber/api/controller/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(UserController.class)
+@ActiveProfiles("test")
 class UserControllerTest {
 
     @Autowired
@@ -171,4 +173,3 @@ class UserControllerTest {
     }
 
 }
-
